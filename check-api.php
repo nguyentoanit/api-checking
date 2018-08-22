67ea83751326b48b1f33b43d1135abe23c4b9760
Toan Nguyen <nguyentoandev@gmail.com>
2018-08-22 15:57:38 +0700

    Commit 1

--- check-api.php
+++ check-api.php
@@ -41,7 +41,7 @@ if (!$apis){
 // Initial Guzzle Client
 $client = new GuzzleHttp\Client(['base_uri' => $apis['base_uri']]);
 
-//Loop the resource
+//Lo
 foreach ($apis['api'] as $api) {
     try {
         $response = $client->request($api['method'], $api['endpoint'],[
