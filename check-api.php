8db3182e1404fa1a21b9819b0462a060335eb792
Toan Nguyen <nguyentoandev@gmail.com>
2018-08-22 08:49:02 +0700

    Remove old log folder

--- check-api.php
+++ check-api.php
@@ -5,12 +5,22 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7;
 use GuzzleHttp\Exception\RequestException;
 
-//Check folder
-$dir = date("Y_m_d");
+//Check and create folder log
+$today = date("Y-m-d");
+$threeDayAgo = "log";
 
-if (!is_dir($dir)) {
-    mkdir($dir);
+if (!is_dir($today)) {
+    mkdir($today);
+    
+    $threeDayAgo = date_create($today);
+    date_sub($threeDayAgo,date_interval_create_from_date_string("3 days"));
+    $threeDayAgo =  date_format($threeDayAgo,"Y-m-d");
 }
 
+//Check and remove old log folder
+if (is_dir($threeDayAgo)) {
+    array_map('unlink', glob("$threeDayAgo/*.*"));
+    rmdir($threeDayAgo);
+}
 
 ?>
